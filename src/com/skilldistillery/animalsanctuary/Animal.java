14f47a7d04853b24edb35ab9d973f53d04556133
Yola Rivers <yolarivers@gmail.com>
2024-06-30 17:06:58 -0500

    added constructors and getters

--- src/com/skilldistillery/animalsanctuary/Animal.java
+++ src/com/skilldistillery/animalsanctuary/Animal.java
@@ -2,13 +2,27 @@ package com.skilldistillery.animalsanctuary;
 
 public class Animal {
 	private String name;
-
-	public String getName() {
-		return name;
+	private String species;
+    private String color;
+    
+    
+    public Animal(String name, String species, String color) {
+        this.name = name;
+        this.species = species;
+        this.color = color;
+        
 	}
+	
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getColor() {
+        return color;
+    }
 
 }
